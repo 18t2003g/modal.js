644408b522f8bc4df1a932db891671f4afdf4ceb
Kenta Asahi <18t2003g@shinshu-u.ac.jp>
2021-11-16 16:41:05 +0900

    ver0.1alpha

--- modal.js
+++ modal.js
@@ -2,5 +2,51 @@
 *  modal.js Version 0.1
 *  The MIT Licence
 *  (C) 2021 Kenta Asahi
-*  
-*/
+*  https://github.com/18t2003g/modal.js
+*  ===========================================*/
+
+//shadow
+const shadowDiv = document.createElement("div");
+shadowDiv.setAttribute("id", "modal-js-shadow");
+shadowDiv.style.zIndex = "254";
+shadowDiv.style.position = "fixed";
+shadowDiv.style.visibility = "hidden";
+shadowDiv.style.position = "fixed";
+shadowDiv.style.top = "0";
+shadowDiv.style.left = "0";
+shadowDiv.style.width = "100vw";
+shadowDiv.style.height = "100vh";
+shadowDiv.style.backgroundColor = "#000";
+shadowDiv.style.opacity = "0.5";
+
+
+class Modal{
+    constructor(obj){
+        this.DOMobj = obj;
+        this.DOMobj.style.visibility = "hidden";
+        this.DOMobj.style.position = "fixed";
+        this.DOMobj.style.zIndex = "255";
+        // this.DOMobj.style.left = "calc((100vw - " + this.DOMobj.style.width + "px) / 2)";
+        // this.DOMobj.style.top = "calc((100vh - " + this.DOMobj.style.height + "px) / 2)";
+        this.DOMobj.style.left = (window.innerWidth - this.DOMobj.style.width) / 2 + "px";
+        this.DOMobj.style.top = (window.innerHeight - this.DOMobj.style.height) / 2 + "px";
+        this.shadow = false;
+    }
+
+    //show modal window
+    show(){
+        if(this.shadow == true){
+            document.body.appendChild(shadowDiv);
+            document.getElementById("modal-js-shadow").style.visibility = "visible";
+        }
+        this.DOMobj.style.visibility = "visible";
+    }
+
+    //hide modal window
+    hide(){
+        if(this.shadow == true){
+            document.getElementById("modal-js-shadow").style.visibility = "hidden";
+        }
+        this.DOMobj.style.visibility = "hidden";
+    }
+}
